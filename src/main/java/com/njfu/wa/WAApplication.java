0fe2b52f2035e4d5766298fcc7520bcc6b82013e
wch <wch853@163.com>
2017-07-23 20:18:19 +0800

    wa project

---
+++ src/main/java/com/njfu/wa/WAApplication.java
@@ -0,0 +1,12 @@
+package com.njfu.wa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WAApplication.class, args);
+	}
+}
