7d0a4554335d8f6aaee3d65e5e034017c8719a07
wch <wch853@163.com>
2017-07-25 21:16:38 +0800

    std.css

---
+++ src/main/java/com/njfu/wa/sys/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.njfu.wa.sys.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
